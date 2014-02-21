a8928dd40784c230d0c219e2a6e871c974be7203
Oscar <oscar.pdr@gmail.com>
2014-02-21 15:41:26 +0100

    Evolución de temática por años

--- website/static/data/cache.py
+++ website/static/data/cache.py
@@ -9,6 +9,9 @@ import pickle
 import gender
 import difflib
 import math
+import os
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
 
 
 def load_config():
@@ -19,7 +22,7 @@ def load_config():
           'host': '127.0.0.1',
           'database': 'teseo',
         }
-        
+
     with open('pass.config', 'r') as inputfile:
         for i, line in enumerate(inputfile):
             if i == 0:
@@ -28,11 +31,11 @@ def load_config():
                 config['password'] = line
             elif i > 1:
                 break
-            
+
     return config
 
 def get_university_ids():
-    import mysql.connector 
+    import mysql.connector
     config = load_config()
     cnx = mysql.connector.connect(**config)
     cursor_unis = cnx.cursor()
@@ -41,11 +44,11 @@ def get_university_ids():
     for university in cursor_unis:
         result[university[0]] = university[1]
     cursor_unis.close()
-    
+
     print result
-    
+
 def save_thesis_ids():
-    import mysql.connector   
+    import mysql.connector
     config = load_config()
     cnx = mysql.connector.connect(**config)
     cursor = cnx.cursor()
@@ -54,17 +57,17 @@ def save_thesis_ids():
     for thesis_id in cursor:
         result.add(thesis_id[0])
     cursor.close()
-    
-    with open( "./cache/thesis_ids.p", "wb" ) as outfile:
+
+    with open( base_dir + "/cache/thesis_ids.p", "wb" ) as outfile:
         pickle.dump(result, outfile)
-        
+
 def load_thesis_ids():
-    with open( "./cache/thesis_ids.p", "rb" ) as infile:
+    with open( base_dir + "/cache/thesis_ids.p", "rb" ) as infile:
         result = pickle.load(infile)
     return result
-    
+
 def save_descriptors():
-    import mysql.connector   
+    import mysql.connector
     config = load_config()
     cnx = mysql.connector.connect(**config)
     cursor = cnx.cursor()
@@ -73,17 +76,17 @@ def save_descriptors():
     for descriptor in cursor:
         result[descriptor[0]] = descriptor[1]
     cursor.close()
-    
-    with open( "./cache/descriptors.p", "wb" ) as outfile:
+
+    with open( base_dir + "/cache/descriptors.p", "wb" ) as outfile:
         pickle.dump(result, outfile)
-        
+
 def load_descriptors():
-    with open( "./cache/descriptors.p", "rb" ) as infile:
+    with open( base_dir + "/cache/descriptors.p", "rb" ) as infile:
         result = pickle.load(infile)
     return result
-        
+
 def get_names():
-    import mysql.connector   
+    import mysql.connector
     config = load_config()
     cnx = mysql.connector.connect(**config)
     cursor = cnx.cursor()
@@ -93,19 +96,19 @@ def get_names():
         first = str(name[0]).split(' ')[0]
         result.add(first)
     cursor.close()
-        
+
     return list(result)
-    
+
 def save_name_genders():
     name_pool = get_names()
-        
+
     result = {}
     bad_names = []
-    
+
     chunk_size = 50
     total_chunks = len(name_pool)/chunk_size
-    rest = len(name_pool)%chunk_size    
-    
+    rest = len(name_pool)%chunk_size
+
     for j in range(0, total_chunks):
         print '*******Chunk', j, '/', total_chunks
         names = []
@@ -113,32 +116,32 @@ def save_name_genders():
             names = name_pool[j * chunk_size:total_chunks*chunk_size+rest]
         else:
             names = name_pool[j * chunk_size:(j+1)*chunk_size]
-        
-        gender_list = gender.getGenders(names) #gender, prob, count        
-        
+
+        gender_list = gender.getGenders(names) #gender, prob, count
+
         for i, name in enumerate(names):
             infered_gender = gender_list[i][0]
             prob = float(gender_list[i][1])
             print name, infered_gender, prob
-            if infered_gender == 'None' or prob < 0.6: 
+            if infered_gender == 'None' or prob < 0.6:
                 bad_names.append(name)
             result[name] = infered_gender
-        
-    with open( "./cache/genders.p", "wb" ) as outfile:
-        pickle.dump(result, outfile) 
-        
-    with open( "./cache/badnames.p", "wb" ) as outfile:
-        pickle.dump(bad_names, outfile) 
-    
+
+    with open( base_dir + "/cache/genders.p", "wb" ) as outfile:
+        pickle.dump(result, outfile)
+
+    with open( base_dir + "/cache/badnames.p", "wb" ) as outfile:
+        pickle.dump(bad_names, outfile)
+
     return bad_names
-       
+
 def load_genders():
-    with open( "./cache/genders.p", "rb" ) as infile:
+    with open( base_dir + "/cache/genders.p", "rb" ) as infile:
         result = pickle.load(infile)
     return result
-    
+
 def get_complete_names():
-    import mysql.connector   
+    import mysql.connector
     config = load_config()
     cnx = mysql.connector.connect(**config)
     cursor = cnx.cursor()
@@ -148,51 +151,51 @@ def get_complete_names():
         result.append(name[0])
     cursor.close()
     return result
-    
+
 def check_similar_names():
     print 'Getting names'
     names = get_complete_names()
     print 'Total names:', len(names)
     # min similarity ratio between strings
     threshold_ratio = 0.8
-    repeated = []    
+    repeated = []
     count = 0.0
-    
+
     for i, str_1 in enumerate(names):
-        for j in range(i+1, len(names)): 
+        for j in range(i+1, len(names)):
             if count%10000 == 0:
                 print 'Similar', count
             count+=1
             str_2 = names[j]
-            
+
             if (difflib.SequenceMatcher(None, str_1, str_2).ratio() > threshold_ratio):
                 print 'Similar', str_1, str_2
                 repeated.append((str_1, str_2))
-    
-    with open( "./cache/repeated.p", "wb" ) as outfile:
+
+    with open( base_dir + "/cache/repeated.p", "wb" ) as outfile:
         result = pickle.dump(outfile)
-            
+
     return repeated
-            
+
 
 def load_descriptor_codes():
-    with open( "./cache/descriptor_codes.p", "rb" ) as infile:
+    with open( base_dir + "/cache/descriptor_codes.p", "rb" ) as infile:
         result = pickle.load(infile)
     return result
-    
+
 def load_codes_descriptor():
-    with open( "./cache/descriptor_codes.p", "rb" ) as infile:
+    with open( base_dir + "/cache/descriptor_codes.p", "rb" ) as infile:
         result = pickle.load(infile)
-        
+
     inverted_result = {}
     for name in result:
         code = result[name]
         inverted_result[code] = name
-        
+
     return inverted_result
-        
-            
-    
+
+
+
 
 
 
@@ -293,92 +296,92 @@ university_locations = {
 }
 
 university_ids = {
-    1: u'SANTIAGO DE COMPOSTELA', 
-    2: u'AUT\xd3NOMA DE BARCELONA', 
-    3: u'UNIVERSITAT DE VAL\xc8NCIA (ESTUDI GENERAL)', 
-    4: u'COMPLUTENSE DE MADRID', 
-    5: u'OVIEDO', 
-    6: u'AUT\xd3NOMA DE MADRID', 
-    7: u'PA\xcdS VASCO/EUSKAL HERRIKO UNIBERTSITATEA', 
-    8: u'GRANADA', 
-    9: u'NACIONAL DE EDUCACI\xd3N A DISTANCIA', 
-    10: u'BURGOS', 
-    11: u'NAVARRA', 
-    12: u'ALICANTE', 
-    13: u'ROVIRA I VIRGILI', 
-    14: u'POLIT\xc9CNICA DE VALENCIA', 
-    15: u'SEVILLA', 
-    16: u'EXTREMADURA', 
-    17: u'ZARAGOZA', 
-    18: u'POMPEU FABRA', 
-    19: u'POLIT\xc9CNICA DE MADRID', 
-    20: u'M\xc1LAGA', 
-    21: u'POLIT\xc9CNICA DE CATALUNYA', 
-    22: u'MIGUEL HERN\xc1NDEZ DE ELCHE', 
-    23: u'RIOJA', 
-    24: u'CARLOS III DE MADRID', 
-    25: u'GIRONA', 
-    26: u'BARCELONA', 
-    27: u'VIGO', 
-    28: u'SALAMANCA', 
-    29: u'MURCIA', 
-    30: u'P\xdaBLICA DE NAVARRA', 
-    31: u'VALLADOLID', 
-    32: u'PALMAS DE GRAN CANARIA', 
-    33: u'ALMER\xcdA', 
-    34: u'LA LAGUNA', 
-    35: u'LLEIDA', 
-    36: u'C\xd3RDOBA', 
-    37: u'C\xc1DIZ', 
-    38: u'ILLES BALEARS', 
-    39: u'ABAT OLIBA CEU', 
-    40: u'ALCAL\xc1', 
-    41: u'DEUSTO', 
-    42: u'EUROPEA DE MADRID', 
-    43: u'CANTABRIA', 
-    44: u'JA\xc9N', 
-    45: u'PONTIFICIA DE SALAMANCA', 
-    46: u'REY JUAN CARLOS', 
-    47: u'LE\xd3N', 
-    48: u'RAM\xd3N LLULL', 
-    49: u'POLIT\xc9CNICA DE CARTAGENA', 
-    50: u'PONTIFICIA COMILLAS', 
-    51: u'CASTILLA-LA MANCHA', 
-    52: u'JAUME I DE CASTELL\xd3N', 
-    53: u'CAT\xd3LICA DE VALENCIA SAN VICENTE M\xc1RTIR', 
-    54: u'A CORU\xd1A', 
-    55: u'PABLO DE OLAVIDE', 
-    56: u'SAN PABLO-CEU', 
-    57: u'HUELVA', 
-    58: u'CARDENAL HERRERA-CEU', 
-    59: u'OBERTA DE CATALUNYA', 
-    60: u'CAT\xd3LICA SAN ANTONIO', 
-    61: u'INTERNACIONAL DE CATALUNYA', 
-    62: u'ANTONIO DE NEBRIJA', 
-    63: u'MONDRAG\xd3N UNIBERTSITATEA', 
-    64: u'FRANCISCO DE VITORIA', 
-    65: u'CAMILO JOS\xc9 CELA', 
-    66: u'IE UNIVERSITY', 
-    67: u'INTERNACIONAL MEN\xc9NDEZ PELAYO', 
-    68: u'VIC', 
-    69: u'INTERNACIONAL DE VALENCIA', 
-    70: u'ALFONSO X EL SABIO', 
-    71: u'A DISTANCIA DE MADRID', 
-    72: u'CAT\xd3LICA SANTA TERESA DE JES\xdaS DE \xc1VILA', 
-    73: u'SAN JORGE', 
-    74: u'INTERNACIONAL DE ANDALUC\xcdA', 
-    75: u'EUROPEA MIGUEL DE CERVANTES', 
+    1: u'SANTIAGO DE COMPOSTELA',
+    2: u'AUT\xd3NOMA DE BARCELONA',
+    3: u'UNIVERSITAT DE VAL\xc8NCIA (ESTUDI GENERAL)',
+    4: u'COMPLUTENSE DE MADRID',
+    5: u'OVIEDO',
+    6: u'AUT\xd3NOMA DE MADRID',
+    7: u'PA\xcdS VASCO/EUSKAL HERRIKO UNIBERTSITATEA',
+    8: u'GRANADA',
+    9: u'NACIONAL DE EDUCACI\xd3N A DISTANCIA',
+    10: u'BURGOS',
+    11: u'NAVARRA',
+    12: u'ALICANTE',
+    13: u'ROVIRA I VIRGILI',
+    14: u'POLIT\xc9CNICA DE VALENCIA',
+    15: u'SEVILLA',
+    16: u'EXTREMADURA',
+    17: u'ZARAGOZA',
+    18: u'POMPEU FABRA',
+    19: u'POLIT\xc9CNICA DE MADRID',
+    20: u'M\xc1LAGA',
+    21: u'POLIT\xc9CNICA DE CATALUNYA',
+    22: u'MIGUEL HERN\xc1NDEZ DE ELCHE',
+    23: u'RIOJA',
+    24: u'CARLOS III DE MADRID',
+    25: u'GIRONA',
+    26: u'BARCELONA',
+    27: u'VIGO',
+    28: u'SALAMANCA',
+    29: u'MURCIA',
+    30: u'P\xdaBLICA DE NAVARRA',
+    31: u'VALLADOLID',
+    32: u'PALMAS DE GRAN CANARIA',
+    33: u'ALMER\xcdA',
+    34: u'LA LAGUNA',
+    35: u'LLEIDA',
+    36: u'C\xd3RDOBA',
+    37: u'C\xc1DIZ',
+    38: u'ILLES BALEARS',
+    39: u'ABAT OLIBA CEU',
+    40: u'ALCAL\xc1',
+    41: u'DEUSTO',
+    42: u'EUROPEA DE MADRID',
+    43: u'CANTABRIA',
+    44: u'JA\xc9N',
+    45: u'PONTIFICIA DE SALAMANCA',
+    46: u'REY JUAN CARLOS',
+    47: u'LE\xd3N',
+    48: u'RAM\xd3N LLULL',
+    49: u'POLIT\xc9CNICA DE CARTAGENA',
+    50: u'PONTIFICIA COMILLAS',
+    51: u'CASTILLA-LA MANCHA',
+    52: u'JAUME I DE CASTELL\xd3N',
+    53: u'CAT\xd3LICA DE VALENCIA SAN VICENTE M\xc1RTIR',
+    54: u'A CORU\xd1A',
+    55: u'PABLO DE OLAVIDE',
+    56: u'SAN PABLO-CEU',
+    57: u'HUELVA',
+    58: u'CARDENAL HERRERA-CEU',
+    59: u'OBERTA DE CATALUNYA',
+    60: u'CAT\xd3LICA SAN ANTONIO',
+    61: u'INTERNACIONAL DE CATALUNYA',
+    62: u'ANTONIO DE NEBRIJA',
+    63: u'MONDRAG\xd3N UNIBERTSITATEA',
+    64: u'FRANCISCO DE VITORIA',
+    65: u'CAMILO JOS\xc9 CELA',
+    66: u'IE UNIVERSITY',
+    67: u'INTERNACIONAL MEN\xc9NDEZ PELAYO',
+    68: u'VIC',
+    69: u'INTERNACIONAL DE VALENCIA',
+    70: u'ALFONSO X EL SABIO',
+    71: u'A DISTANCIA DE MADRID',
+    72: u'CAT\xd3LICA SANTA TERESA DE JES\xdaS DE \xc1VILA',
+    73: u'SAN JORGE',
+    74: u'INTERNACIONAL DE ANDALUC\xcdA',
+    75: u'EUROPEA MIGUEL DE CERVANTES',
     76: u'INTERNACIONAL DE LA RIOJA'
 }
 
 
 
 
-#this should be done the first time running this scripts    
+#this should be done the first time running this scripts
 if __name__=='__main__':
     import pprint
-    pp = pprint.PrettyPrinter(indent=4) 
+    pp = pprint.PrettyPrinter(indent=4)
     pp.pprint(descriptor_codes)
-    
+
 
 
--- website/teseo.py
+++ website/teseo.py
@@ -4,6 +4,7 @@
 # all the imports
 from flask import Flask, request, session, g, redirect, url_for, render_template, flash
 import json
+from static.data.cache import codes_descriptor
 
 
 app = Flask(__name__)
@@ -25,19 +26,19 @@ def total_theses_by_year():
     return render_template("total_theses_by_year.html")
 
 
-@app.route('/topics/<year>')
-def topics_by_five_years(year):
+@app.route('/topics/<min_year>/<max_year>')
+def topics_by_five_years(min_year, max_year):
     json_data = open('static/data/processed_data/first_level_areas_temporal.json')
     data = json.load(json_data)
     json_data.close
 
     tag_dict = {}
 
-    min_year = int(year)
-    max_year = min_year + 5
+    min_year = int(min_year)
+    max_year = int(max_year)
 
     for _year in range(min_year, max_year):
-        tags = data[str(year)]
+        tags = data[str(_year)]
         for key, value in tags.items():
             if key in tag_dict.keys():
                 tag_dict[key] = tag_dict[key] + value
@@ -47,19 +48,19 @@ def topics_by_five_years(year):
     return render_template("topics_per_five_years.html", tag_dict=tag_dict, min_year=min_year, max_year=max_year)
 
 
-@app.route('/all_topics/<year>')
-def all_topics_by_five_years(year):
+@app.route('/all_topics/<min_year>/<max_year>')
+def all_topics_by_five_years(min_year, max_year):
     json_data = open('static/data/processed_data/areas_temporal.json')
     data = json.load(json_data)
     json_data.close
 
     tag_dict = {}
 
-    min_year = int(year)
-    max_year = min_year + 5
+    min_year = int(min_year)
+    max_year = int(max_year)
 
     for _year in range(min_year, max_year):
-        tags = data[str(year)]
+        tags = data[str(_year)]
         for key, value in tags.items():
             if key in tag_dict.keys():
                 tag_dict[key] = tag_dict[key] + value
@@ -67,6 +68,22 @@ def all_topics_by_five_years(year):
                 tag_dict[key] = value
 
     return render_template("topics_per_five_years.html", tag_dict=tag_dict, min_year=min_year, max_year=max_year)
+
+
+@app.route('/evolution_topic/<topic>')
+def evolution_topic(topic):
+    return render_template("evolution_topic.html", topic=topic)
+
+
+@app.route('/evolution_topic/')
+def evolution_topic_list():
+    topics = []
+
+    for key, value in codes_descriptor.items():
+        if str(key)[2:6] == '0000':
+            topics.append(value)
+
+    return render_template("evolution_topic_list.html", topics=topics)
 
 
 if __name__ == '__main__':
