bc5d8313623cc45b3092898efb6bb6a6d1bba66c
Oscar <oscar.pdr@gmail.com>
2014-02-21 13:48:03 +0100

    Topics tag cloud visualizations

--- website/teseo.py
+++ website/teseo.py
@@ -3,6 +3,7 @@
 
 # all the imports
 from flask import Flask, request, session, g, redirect, url_for, render_template, flash
+import json
 
 
 app = Flask(__name__)
@@ -22,6 +23,50 @@ def gender():
 @app.route('/total')
 def total_theses_by_year():
     return render_template("total_theses_by_year.html")
+
+
+@app.route('/topics/<year>')
+def topics_by_five_years(year):
+    json_data = open('static/data/processed_data/first_level_areas_temporal.json')
+    data = json.load(json_data)
+    json_data.close
+
+    tag_dict = {}
+
+    min_year = int(year)
+    max_year = min_year + 5
+
+    for _year in range(min_year, max_year):
+        tags = data[str(year)]
+        for key, value in tags.items():
+            if key in tag_dict.keys():
+                tag_dict[key] = tag_dict[key] + value
+            else:
+                tag_dict[key] = value
+
+    return render_template("topics_per_five_years.html", tag_dict=tag_dict, min_year=min_year, max_year=max_year)
+
+
+@app.route('/all_topics/<year>')
+def all_topics_by_five_years(year):
+    json_data = open('static/data/processed_data/areas_temporal.json')
+    data = json.load(json_data)
+    json_data.close
+
+    tag_dict = {}
+
+    min_year = int(year)
+    max_year = min_year + 5
+
+    for _year in range(min_year, max_year):
+        tags = data[str(year)]
+        for key, value in tags.items():
+            if key in tag_dict.keys():
+                tag_dict[key] = tag_dict[key] + value
+            else:
+                tag_dict[key] = value
+
+    return render_template("topics_per_five_years.html", tag_dict=tag_dict, min_year=min_year, max_year=max_year)
 
 
 if __name__ == '__main__':
