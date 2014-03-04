c9ebfe708bf7dd052a81d0f83849994caed3a37d
Aitor Almeida <aitoralmeida@gmail.com>
2014-03-04 12:14:39 +0100

    minor fix

--- data/database_cleaner.py
+++ data/database_cleaner.py
@@ -93,9 +93,14 @@ def get_repeated_thesis_ids(distinct_titles):
         for thesis_id in cursor:
             name_ids.append(thesis_id[0])
         if len(name_ids) > 1:
+            print 'Repeated ids', name_ids
             repeated_ids.append(name_ids)
             
     cursor.close()
+    
+    with open( base_dir + "/cache/repeated_thesis_ids.json", "wb" ) as outfile:
+        json.dump(repeated_ids, outfile)
+
     return repeated_ids
     
 #def merge_repeated_thesis():
