314398d6a7bec08fedc50d64c6f979b429423ee7
Oscar Peña <oscar.pdr@gmail.com>
2014-03-04 12:57:39 +0100

    Navigation enabled and index&about sections added (text to be included)

--- website/teseo.py
+++ website/teseo.py
@@ -40,6 +40,11 @@ def index():
     return render_template("index.html")
 
 
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
 #####   Gender analysis     ###############################################
 
 
