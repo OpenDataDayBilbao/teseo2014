a7c67a9d6bed13f5cc7ca6104d759828a8f80edb
Aitor Almeida <aitoralmeida@gmail.com>
2014-03-18 09:31:30 +0100

    added method to set genders

--- data/database_cleaner.py
+++ data/database_cleaner.py
@@ -312,8 +312,7 @@ def set_genders():
     cnx = mysql.connector.connect(**config)
     cursor = cnx.cursor()
     for cont, first_name in enumerate(names):
-        if cont%500 == 0:
-            print 'Setting genders:', first_name, (cont/len(names)) * 100
+        print 'Setting genders:', first_name, (float(cont)/len(names)) * 100
             
         name = first_name[0].split(' ')[0]
         try:
