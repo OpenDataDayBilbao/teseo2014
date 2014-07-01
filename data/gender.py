958c67ea4765a8dc076cb4a3bf681e8670565e0b
Aitor Almeida <aitoralmeida@gmail.com>
2014-07-01 09:03:53 +0200

    changed how the results are returned

--- data/gender.py
+++ data/gender.py
@@ -15,13 +15,15 @@ def getGenders(names):
 			url = url + "&name[" + str(cnt) + "]=" + name
 		cnt += 1
 
+  
 	req = requests.get("http://api.genderize.io?" + url)
 	results = json.loads(req.text)
 	
 	retrn = []
 	for result in results:
 		if result["gender"] != None:
-			retrn.append((result["gender"], result["probability"], result["count"]))
+			retrn.append((result['name'], result["gender"], result["probability"], result["count"]))
 		else:
 			retrn.append((u'None',u'0.0',0.0))
 	return retrn
+ 
